739b864c1499ccdbf9010d8fe774087a07bb09ee
Nicolas Barral <n.barral@imperial.ac.uk>
2018-10-21 21:35:35 +0100

    follow main repo

--- lecture2/lecture.py
+++ lecture2/lecture.py
@@ -4,3 +4,19 @@ from client.utils import auth
 
 args = assignment.Settings(server='clewolffautook21.eastus.cloudapp.azure.com/okpy')
 ok = Notebook('./lecture.ok', args)
+
+# This cell sets the css styles for the rest of the notebook.
+# Unless you are particlarly interested in that kind of thing, you can safely ignore it
+
+from IPython.core.display import HTML
+
+def css_styling():
+    styles = """<style>
+div.exercise {    
+    background-color: #e6f0ff;
+    border-left: 5px solid #003380;
+    padding: 0.5em;
+    }
+ </style>
+"""
+    return HTML(styles)
